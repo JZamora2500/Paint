22eb15453c580d17e36361cec0dde19342491dc7
JZamora2500 <101601677+JZamora2500@users.noreply.github.com>
2022-12-14 20:55:40 -0600

    Add files via upload

---
+++ Paint/src/paint/Paint.java
@@ -0,0 +1,14 @@
+
+package paint;
+
+public class Paint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       interfaz in=new interfaz();
+        in.setVisible(true);
+        in.setLocationRelativeTo(null);
+    }
+}
